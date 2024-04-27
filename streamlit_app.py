c5e4912275942d1098263162537a67d200586066
hernah <hgalosada@gmail.com>
2024-04-27 16:11:33 +0800

    changed text_input to number_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # Load the trained model
-model = pickle.load(open('triage_classification (4).pkl', 'rb'))
+model = pickle.load(open('triage_classification (5).pkl', 'rb'))
 
 st.title("Triage Classifier")
 
@@ -12,31 +13,34 @@ st.write("""
 A triage classifier that uses machine learning to distinguish the triage color from the given symptoms. 
 """)
 
-# Input fields
-age = st.text_input("Enter age: ")
-bmi = st.text_input("Enter BMI (Range: 15-50): ")
-bp = st.text_input("Enter blood pressure (Range: 80-200): ")
-chol = st.text_input("Enter cholesterol (Range: 100-500): ")
-plasma_glucose = st.text_input("Enter plasma glucose (Range: 50-400): ")
-exercise_angina_str = st.radio("Do you experience exercise angina (chest pain during physical exercise)?", ("No", "Yes"))
+input_data = None
+
+bmi_range = (15, 50)
+bp_range = (80, 200)
+chol_range = (100, 500)
+plasma_glucose_range = (50, 400)
+
+age = st.number_input("Enter age: ", value=None, placeholder=15, key='age', min_value=10, max_value=150)
+bmi = st.number_input("Enter BMI (Range: 15-50): ", key='bmi', value=None,placeholder=20, min_value=bmi_range[0], max_value=bmi_range[1])
+bp = st.number_input("Enter blood pressure (Range: 80-200): ", value=None,placeholder=80, key='bp', min_value=bp_range[0], max_value=bp_range[1])
+chol = st.number_input("Enter cholesterol (Range: 100-500): ",value=None,placeholder=100,  key='chol', min_value=chol_range[0], max_value=chol_range[1])
+plasma_glucose = st.number_input("Enter plasma glucose (Range: 50-400): ", value=None,placeholder=50, key='plasma_glucose', min_value=plasma_glucose_range[0], max_value=plasma_glucose_range[1])
+exercise_angina_str = st.radio("Do you experience exercise angina (chest pain during physical exercise)?", ("No", "Yes"), index=0)
 exercise_angina = 1 if exercise_angina_str == "Yes" else 0
 cp_type_options = ["No Pain (0)", "Mild (1)", "Moderate (2)", "Severe (3)", "Very Severe (4)"]
 cp_type_str = st.selectbox("Select chest pain type:", cp_type_options, index=0)
 cp_type_mapping = {"No Pain (0)": 0, "Mild (1)": 1, "Moderate (2)": 2, "Severe (3)": 3, "Very Severe (4)": 4}
 cp_type = cp_type_mapping.get(cp_type_str)
-input_data = None
-# Convert input data to appropriate types and validate input
+
+# Validate input
 if age and bmi and bp and chol and plasma_glucose:
-    input_data = np.array([[float(age), float(bmi), float(bp), float(chol), float(plasma_glucose), exercise_angina, cp_type]])
+    # 'chest_pain_type', 'cholesterol', 'exercise_angina', 'blood pressure', 'age', 'plasma glucose', 'bmi'
+    input_data = np.array([[cp_type, float(chol),  exercise_angina, float(bp), float(age), float(plasma_glucose), float(bmi)]])
 else:
     st.warning("Please fill in all the input fields.")
 
 # Make prediction if input is valid
-if input_data is not None:
-    if st.button('Predict'):
-        prediction = model.predict(input_data)
-        predicted_color = np.unique(prediction, return_counts=True)[0][np.argmax(np.unique(prediction, return_counts=True)[1])]
-        triage_mapping = {'green': 0, 'yellow': 1, 'orange': 2, 'red': 3}
-        triage_mapping_inv = {v: k for k, v in triage_mapping.items()}
-        predicted_color_label = triage_mapping_inv[predicted_color]
-        st.write(f"Predicted triage color: {predicted_color_label}")
+if st.button('Predict'):
+    prediction = model.predict(input_data)
+    triage_order = {0: 'Green', 1: 'Yellow', 2: 'Orange', 3: 'Red'}
+    st.write(f"Predicted triage color: {triage_order[prediction[0]]}")
